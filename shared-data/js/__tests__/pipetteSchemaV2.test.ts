74312f2190d662bafd51cb6b73f95687da444c70
Ryan Howard <ryan.howard@opentrons.com>
2024-11-12 11:13:45 -0500

    feat(shared-data): add lv96 shared data definitions (#16749)
    <!--
    Thanks for taking the time to open a Pull Request (PR)! Please make sure
    you've read the "Opening Pull Requests" section of our Contributing
    Guide:
    
    
    https://github.com/Opentrons/opentrons/blob/edge/CONTRIBUTING.md#opening-pull-requests
    
    GitHub provides robust markdown to format your PR. Links, diagrams,
    pictures, and videos along with text formatting make it possible to
    create a rich and informative PR. For more information on GitHub
    markdown, see:
    
    
    https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
    
    To ensure your code is reviewed quickly and thoroughly, please fill out
    the sections below to the best of your ability!
    -->
    
    # Overview
    
    Add 200ul pipette definitions to shared data
    <!--
    Describe your PR at a high level. State acceptance criteria and how this
    PR fits into other work. Link issues, PRs, and other relevant resources.
    -->
    
    ## Test Plan and Hands on Testing
    
    <!--
    Describe your testing of the PR. Emphasize testing not reflected in the
    code. Attach protocols, logs, screenshots and any other assets that
    support your testing.
    -->
    
    ## Changelog
    
    <!--
    List changes introduced by this PR considering future developers and the
    end user. Give careful thought and clear documentation to breaking
    changes.
    -->
    
    ## Review requests
    
    <!--
    - What do you need from reviewers to feel confident this PR is ready to
    merge?
    - Ask questions.
    -->
    
    ## Risk assessment
    This will not work out of the box until we get aspirate/dispense pipette
    functions from the factory.
    <!--
    - Indicate the level of attention this PR needs.
    - Provide context to guide reviewers.
    - Discuss trade-offs, coupling, and side effects.
    - Look for the possibility, even if you think it's small, that your
    change may affect some other part of the system.
    - For instance, changing return tip behavior may also change the
    behavior of labware calibration.
    - How do your unit tests and on hands on testing mitigate this PR's
    risks and the risk of future regressions?
    - Especially in high risk PRs, explain how you know your testing is
    enough.
    -->

--- shared-data/js/__tests__/pipetteSchemaV2.test.ts
+++ shared-data/js/__tests__/pipetteSchemaV2.test.ts
@@ -50,7 +50,7 @@ describe('test schema against all liquid specs definitions', () => {
     })
 
     it(`second parent dir matches pipette model: ${liquidPath}`, () => {
-      expect(['p10', 'p20', 'p50', 'p300', 'p1000']).toContain(
+      expect(['p10', 'p20', 'p50', 'p200', 'p300', 'p1000']).toContain(
         path.basename(path.dirname(path.dirname(liquidPath)))
       )
     })
@@ -73,7 +73,7 @@ describe('test schema against all geometry specs definitions', () => {
     })
 
     it(`parent dir matches pipette model: ${geometryPath}`, () => {
-      expect(['p10', 'p20', 'p50', 'p300', 'p1000']).toContain(
+      expect(['p10', 'p20', 'p50', 'p200', 'p300', 'p1000']).toContain(
         path.basename(path.dirname(geometryPath))
       )
     })
@@ -100,7 +100,7 @@ describe('test schema against all general specs definitions', () => {
     })
 
     it(`parent dir matches pipette model: ${generalPath}`, () => {
-      expect(['p10', 'p20', 'p50', 'p300', 'p1000']).toContain(
+      expect(['p10', 'p20', 'p50', 'p200', 'p300', 'p1000']).toContain(
         path.basename(path.dirname(generalPath))
       )
     })
