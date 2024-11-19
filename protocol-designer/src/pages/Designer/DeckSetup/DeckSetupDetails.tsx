0814d91df42ab0877028e5dfc1227bb27133a799
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2024-11-19 09:44:07 -0500

    fix(protocol-designer): default unknown TC lid state to closed (#16838)
    Latest designs specify that the thermocycler lid position should be
    default to closed at the Protocol Steps tab (all upstream steps of the
    first thermocycler step).
    
    [reference](https://github.com/Opentrons/opentrons/pull/16826)

--- protocol-designer/src/pages/Designer/DeckSetup/DeckSetupDetails.tsx
+++ protocol-designer/src/pages/Designer/DeckSetup/DeckSetupDetails.tsx
@@ -200,8 +200,7 @@ export function DeckSetupDetails(props: DeckSetupDetailsProps): JSX.Element {
         }
         const isLabwareOccludedByThermocyclerLid =
           moduleOnDeck.type === THERMOCYCLER_MODULE_TYPE &&
-          (moduleOnDeck.moduleState as ThermocyclerModuleState).lidOpen ===
-            false
+          (moduleOnDeck.moduleState as ThermocyclerModuleState).lidOpen !== true
 
         const tempInnerProps = getModuleInnerProps(moduleOnDeck.moduleState)
         const innerProps =
@@ -210,9 +209,9 @@ export function DeckSetupDetails(props: DeckSetupDetailsProps): JSX.Element {
                 ...tempInnerProps,
                 lidMotorState:
                   (tempInnerProps as ThermocyclerVizProps).lidMotorState !==
-                  'closed'
-                    ? 'open'
-                    : 'closed',
+                  'open'
+                    ? 'closed'
+                    : 'open',
               }
             : tempInnerProps
 
